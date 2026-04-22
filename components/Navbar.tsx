4e1c61d23e557160a9d1770fc68b3ae2b8c500f1
Snippet <aladejanamajid48@gmail.com>
2026-04-22 15:26:02 +0100

    changed resume

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,7 +32,7 @@ const navLinks: Navlinks[] = [
 
 // Resume Google Drive link
 export const resume =
-  "https://drive.google.com/file/d/1LC5gftgP_orGCX0t-KhQwCIfho3RYv6-/view?usp=sharing";
+  "https://drive.google.com/file/d/1RYz4OvRu43EWypdzS_-K-pjz6gdBLjDY/view?usp=sharing";
 
 // Navbar component
 const Navbar: FC = () => {
